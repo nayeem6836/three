9d5155ccc08b608d2772c92af54191f4fcf599a8
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-07-13 20:07:25 +0530

    fouthcommit

---
+++ Team.java
@@ -0,0 +1,11 @@
+public class Team {
+    public void nayeem(){
+        System.out.println("nayeem");
+    }
+    public void preetham(){
+    System.out.println("preetham");
+    }
+    public void asthik()[
+    System.out.println("asthik");
+    ]
+}
